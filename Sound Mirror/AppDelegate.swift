62e0cc04ce50c163161a87c3e653b56f99eaa2ac
Alexey Pyzhianov <alex.pyzhianov@gmail.com>
2020-05-31 15:29:16 +0300

    Remove window stuff

--- Sound Mirror/AppDelegate.swift
+++ Sound Mirror/AppDelegate.swift
@@ -24,16 +24,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
           button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
           button.action = #selector(printQuote(_:))
         }
-        
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
