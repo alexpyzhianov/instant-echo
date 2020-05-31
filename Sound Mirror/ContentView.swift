41f69aa79ce5f2c0bc1e27b1877d788c57106324
Alexey Pyzhianov <alex.pyzhianov@gmail.com>
2020-05-31 15:04:31 +0300

    Initial Commit

---
+++ Sound Mirror/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Sound Mirror
+//
+//  Created by Alexey Pyzhianov on 31.05.2020.
+//  Copyright © 2020 Alex Pyzhianov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
